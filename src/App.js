53785d39fbc25c529c775b5480e7040d549438ea
SayantanStackflow <sayantan.college6291@gmail.com>
2023-01-03 14:58:59 +0000

    added react-spring and subheading css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import React,{useState, useEffect} from 'react';
+import React from 'react';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import Navber from './navber';
 import Postcard from './postcard';
 import './App.css';
 
 function App(){
-  useEffect(() => {
-    const viewport = document.querySelector('meta[name=viewport]');
-    viewport.setAttribute('content', 'width=device-width, initial-scale=0.9, maximum-scale=1.2, minimum-scale=0.8, user-scalable=no');
-  }, []);
-
-  const [offset,setOffset]= useState()
-  const handleScroll= ()=> setOffset(window.pageYOffset)
-
-  window.addEventListener('scroll', handleScroll)
-
 
   return (
   <div className='App'>
@@ -27,12 +17,14 @@ function App(){
       </ParallaxLayer>
       
       <ParallaxLayer offset={0} speed={0.30}>
-        <div className='scroller flex place-content-center lg:mt-[16%] mt-44'>
-          <span className= "blockcontain font-medium leading-loose text-center text-3xl lg:text-5xl font-sans ">
-          Share stories
-          <br></br>
-          Make The Difference<br></br> You Want To Make
+        <div className='scroller flex place-content-center lg:mt-[12%] mt-44'>
+          <span className= "blockcontain tracking-tight leading-loose text-center text-3xl lg:text-4xl ">
+          <p className='Lexend text-[#5F2131]'>Share <span className='satisfy text-[#50183C]'> stories</span></p>
+          <p className='Lexend text-[#652731]'>Make The <span className='text-[#3B1029]'>Difference</span></p>
+          <p className='Lexend text-[#4F183C]'>You Want To Make</p>
+          
           </span>
+          <div className=' pt-20 bg-gradient-to-r from-transparent to-[#FAFCD4] w-[120px] h-[270px] rotate-90'></div>
         </div>
       </ParallaxLayer>
 
@@ -43,7 +35,7 @@ function App(){
       <ParallaxLayer offset={0} speed={0.4}>
         <div className='mountain_2 bg-cover animation_layer parallax'> </div>
       </ParallaxLayer>
-
+      
       
       <ParallaxLayer offset={0} speed={0.5}>
         <div className='mountain_3 bg-cover animation_layer parallax'> </div>
@@ -70,14 +62,17 @@ function App(){
       <ParallaxLayer offset={0} speed={0.35}>
         <div className='traveller bg-cover animation_layer parallaxer'> </div>
       </ParallaxLayer>
-
-      <ParallaxLayer offset={0} speed={0.25}>
-      <Navber/>
-      </ParallaxLayer>
+      
+      
 
       <ParallaxLayer offset={0} speed={0.15}>
         <div className='blureffect'> </div>
       </ParallaxLayer>
+
+      <ParallaxLayer offset={0} speed={0.25}>
+      <Navber/>
+      </ParallaxLayer>
+      
 
       <ParallaxLayer offset={1} speed={0.25}>
       <Postcard/>
