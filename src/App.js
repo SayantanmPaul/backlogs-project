9ca8bb74a76a04179bd712a96971bfec3fe63c2d
SayantanStackflow <sayantan.college6291@gmail.com>
2023-01-07 15:49:13 +0000

    added code formatter-prettier

--- src/App.js
+++ src/App.js
@@ -1,97 +1,96 @@
 import React from 'react';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import {FiChevronsDown} from 'react-icons/fi';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { FiChevronsDown } from 'react-icons/fi';
 import { useState, useEffect } from 'react';
 import Navber from './navber';
 import Postcard from './postcard';
 import './App.css';
 
-function App(){
+function App() {
   const [animation, setAnimation] = useState('bounce 0.5s linear infinite');
 
   useEffect(() => {
     setAnimation('bounce 1s ease-in-out 3');
   }, []);
   return (
-  <div className='App'>
-    
-    <Parallax pages={3} style={{ top: '0', left: '0' }} class="animation">
+    <div className="App">
+      <Parallax pages={3} style={{ top: '0', left: '0' }} class="animation">
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div className=" artboard bg-cover animation_layer parallax"></div>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.25}>
-        <div className=' artboard bg-cover animation_layer parallax'>
-        </div>
-      </ParallaxLayer>
-      
-      <ParallaxLayer offset={0} speed={0.30}>
-        <div className='scroller flex place-content-center lg:mt-[12%] mt-44'>
-          <span className= "blockcontain tracking-tight leading-loose text-center text-3xl lg:text-4xl ">
-          <p className='Lexend text-[#5F2131]'>Share <span className='satisfy text-[#50183C]'> stories</span></p>
-          <p className='Lexend text-[#652731]'>Make The <span className='text-[#3B1029]'>Difference</span></p>
-          <p className='Lexend text-[#4F183C]'>You Want To Make</p>
-          
-          </span>
-          <div className=' pt-20 bg-gradient-to-r from-transparent to-[#FAFCD4] w-[120px] h-[270px] rotate-90'></div>
-        </div>
-      </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div className="scroller flex place-content-center lg:mt-[12%] mt-44">
+            <span className="blockcontain tracking-tight leading-loose text-center text-3xl lg:text-4xl ">
+              <p className="Lexend text-[#5F2131]">
+                Share <span className="satisfy text-[#50183C]"> stories</span>
+              </p>
+              <p className="Lexend text-[#652731]">
+                Make The <span className="text-[#3B1029]">Difference</span>
+              </p>
+              <p className="Lexend text-[#4F183C]">You Want To Make</p>
+            </span>
+            <div className=" pt-20 bg-gradient-to-r from-transparent to-[#FAFCD4] w-[120px] h-[270px] rotate-90"></div>
+          </div>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.30}>
-        <div className='mountain_1 bg-cover animation_layer parallax '> </div>
-      </ParallaxLayer>
-      
-      <ParallaxLayer offset={0} speed={0.4}>
-        <div className='mountain_2 bg-cover animation_layer parallax'> </div>
-      </ParallaxLayer>
-      
-      
-      <ParallaxLayer offset={0} speed={0.5}>
-        <div className='mountain_3 bg-[length:1790px_780px] animation_layer parallax'> </div>
-      </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div className="mountain_1 bg-cover animation_layer parallax "> </div>
+        </ParallaxLayer>
 
-      
-      <ParallaxLayer offset={0} speed={0.25}>
-        <div className='ground_1 bg-cover animation_layer parallax'></div>
-      </ParallaxLayer>
-      
-      <ParallaxLayer offset={0} speed={0.15}>
-        <div className='ground_2 bg-cover animation_layer parallax'> </div>
-      </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.4}>
+          <div className="mountain_2 bg-cover animation_layer parallax"> </div>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.35}>
-      <div className='forest_1 bg-cover animation_layer parallax'> </div>
-      </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div className="mountain_3 bg-[length:1790px_780px] animation_layer parallax">
+            {' '}
+          </div>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.35}>
-        <div className='forest_2 bg-cover animation_layer parallax'> </div>
-      </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div className="ground_1 bg-cover animation_layer parallax"></div>
+        </ParallaxLayer>
 
+        <ParallaxLayer offset={0} speed={0.15}>
+          <div className="ground_2 bg-cover animation_layer parallax"> </div>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.35}>
-        <div className='traveller bg-cover animation_layer parallaxer'> </div>
-      </ParallaxLayer>
-      
-      
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div className="forest_1 bg-cover animation_layer parallax"> </div>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.15}>
-        <div className='blureffect'> </div>
-      </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div className="forest_2 bg-cover animation_layer parallax"> </div>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.2}>
-        <div className='flex justify-center lg:pt-[700px] md:pt-[720px] pt-[610px]'><FiChevronsDown className='cursor-pointer' size={30} style={{ animation, color: '#227C70' }}/></div>
-      </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div className="traveller bg-cover animation_layer parallaxer"> </div>
+        </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={0.25}>
-      <Navber/>
-      </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.15}>
+          <div className="blureffect"> </div>
+        </ParallaxLayer>
 
-      
-      <ParallaxLayer offset={1} speed={0.25}>
-      <Postcard/>
-      </ParallaxLayer>
-      
+        <ParallaxLayer offset={0} speed={0.2}>
+          <div className="flex justify-center lg:pt-[700px] md:pt-[720px] pt-[610px]">
+            <FiChevronsDown
+              className="cursor-pointer"
+              size={30}
+              style={{ animation, color: '#227C70' }}
+            />
+          </div>
+        </ParallaxLayer>
 
-    </Parallax>
-    
-  </div>
+        <ParallaxLayer offset={0} speed={0.25}>
+          <Navber />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.25}>
+          <Postcard />
+        </ParallaxLayer>
+      </Parallax>
+    </div>
   );
 }
 export default App;
--- src/postcard.js
+++ src/postcard.js
@@ -1,36 +1,45 @@
-import React from "react";
+import React from 'react';
 import author from './Images/author.jpg';
+import unsplash1 from './Images/unsplash1.jpg';
+import './cssproperties/postcard.css';
 
-function Postcard(){
-    return(
-        <div className= 'wrapper flex max-w-[46rem] h-[10rem] items-center gap-[1rem] cursor-pointer'>
-        <div className= 'postdetail flex-[2.5] flex flex-col'>
-            <div className= 'flex'>
-                <div className= 'grid plaxe-items-center rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]'>
-                <img src={author} alt="Author" style={{ width: '40px', height: '40px' }} />
-
-                </div>
-                <div className='authorname font-semibold'>Reyse Mark</div>
+function Postcard() {
+  return (
+    <div className=" flex justify-center lg:mr-[500px]">
+      <div className="wrapper flex max-w-[46rem] items-center gap-[1rem] cursor-pointer">
+        <div className="postdetail flex-[2.5] flex flex-col">
+          <div className="flex">
+            <div className="grid plaxe-items-center rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]">
+              <img
+                src={author}
+                alt="Author"
+                style={{ width: '30px', height: '23px' }}
+              />
             </div>
-            <h1 className='font-bold text-2xl'>You should have these extentions in your 
-            vs code right now in 2022</h1>
-            <div className='text-[#696969]'>So you want to make your boring vscode giveing your more
-            productivity, Don't worry I gotcha!</div>
-            <div className='flex items-center justify-between text-[#787878]'>
-            <span className='my-2 text-[.8rem]'>September 13 · 4 min read · 
-                <span className='bg-[#DCDCDC] p-1 rounded-full t'>development</span>
-                </span>
-                <span className='cursor-pointer'>
-                    
-                </span>
-            </div>
+            <div className="authorname pl-1 pb-1">Reyse Mark</div>
+          </div>
+          <h1 className="font-[770] text-[22px] text-[#292929] leading-7 font-poppins heading">
+            You should have these extentions in your vs code 2023
+          </h1>
+          <div className="text-[#696969] hidden lg:block md:block heading">
+            So you want to make your boring vscode giveing your more
+            productivity, Don't worry I gotcha!
+          </div>
+          <div className="flex items-center justify-between text-[#787878]">
+            <span className="my-2 text-[.8rem]">
+              September 13 · 4 min read ·
+              <span className="bg-[#ececec] p-[7px] rounded-full ml-1">
+                development
+              </span>
+            </span>
+            <span className="cursor-pointer"></span>
+          </div>
         </div>
-        <div className='thubnailcontainer'></div>
-        <img src={author} alt="Alt Text" style={{ width: '100px', height: '100px' }} />
-
-        </div>
-        
-    );
+        <div className="thubnailcontainer"></div>
+        <img src={unsplash1} alt="Alt Text" className="w-60 rounded-xl" />
+      </div>
+    </div>
+  );
 }
 
-export default Postcard
+export default Postcard;
